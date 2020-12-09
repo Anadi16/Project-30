33ae62a17bebb3f42f5ca739029d140b512c104f
Anadi16 <71920124+Anadi16@users.noreply.github.com>
2020-12-09 08:29:12 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project-30
+# PROJECT 24
+
---
+++ polygon.js
@@ -0,0 +1,25 @@
+class Poly{
+    constructor(x,y,width,height){
+
+
+        var options = {
+            isStatic : false,
+            restitution : 0
+        }
+        
+
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.image = loadImage("polygon.png");
+        this.width = width;
+        this.height = height;
+
+        World.add(world,this.body);
+    }
+    display(){
+        
+
+        imageMode(CENTER);
+        image(this.image,this.body.position.x,this.body.position.y,this.width,this.height);
+    }
+    
+}
